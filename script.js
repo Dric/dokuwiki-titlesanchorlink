3ca9927ba29d6385a29ad41f0ec17a00ad0b974d
Dric <cedric@driczone.net>
2013-03-05 11:27:46 +0100

    Fixed css bug in script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ jQuery(function () {
 	jQuery(':header').each(function(){
 		var id = jQuery(this).attr('id');
 		var name = jQuery(this).text();
-		jQuery(this).css('position', 'relative');
 		jQuery(this).append('<a title="Link to '+name+'" id="anchor__'+id+'" class="__anchor" href="#'+id+'"><img src="'+DOKU_BASE+'lib/plugins/titlesanchorlink/images/anchor.png" class="__anchor_icon" /></a>');
 	});
 	jQuery(':header').mouseover(function() {
