850df1c2f0515c9a8489be4325ef61a42b1b1a51
Dric <cedric@driczone.net>
2015-12-19 12:46:00 +0100

    Bad copy/paste...

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ jQuery(function () {
 	jQuery(':header').each(function(){
 		var id = jQuery(this).attr('id');
 		if (!id) {
-+            		// https://github.com/Dric/dokuwiki-titlesanchorlink/issues/1
-+            		return;
-+        	}
+           		// https://github.com/Dric/dokuwiki-titlesanchorlink/issues/1
+            		return;
+            	}
 		var name = jQuery(this).text();
 		jQuery(this).append('<a title="Link to '+name+'" id="anchor__'+id+'" class="__anchor" href="#'+id+'"><img src="'+DOKU_BASE+'lib/plugins/titlesanchorlink/images/anchor.png" class="__anchor_icon" /></a>');
 	});
